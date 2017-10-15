f5697a9a73fc79d0d41257cf4b330de1109f75d9
blueberryfox <nastja1905@mail.ru>
2017-10-15 14:57:34 +0500

    added classes Menu and Content

---
+++ php/content.php
@@ -0,0 +1,14 @@
+<?php
+
+class Content
+{
+    public static $pages = array(
+        1 => 'start.html',
+        2 => 'about.html',
+        3 => 'toplist.html'
+    );
+
+    public static function getPage($page_number) {
+        return Content::$pages[$page_number];
+    }
+}
---
+++ php/menu.php
@@ -0,0 +1,23 @@
+<?php
+class Menu {
+    public static $mass = array('Start', 'About', 'Toplist');
+    public static function getMenu($number) {
+        $result_html = '';
+               for ($i = 0; $i < count(Menu::$mass); $i++)
+                    {
+                        $result_html .= '<li class="menu-main__item"';
+                        if ($i == $number) {
+                            $result_html .= ' menu-main__item_active>';
+                        }
+           else {
+               $result_html .= '><a class="menu-main__item_link href="index.php?page=' . $i . '">';
+           }
+             $result_html .= Menu::$mass[$i];
+             if ($i != $number) {
+                 $result_html .= '</a>';
+             }
+             $result_html .= '</li>';
+         }
+         return  $result_html;
+}
+}
